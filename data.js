07ba98045a270bec3656adab25ac6970b7c72951
Harsha <harsha05101@gmail.com>
2025-12-30 12:16:48 +0530

    Updated questions and clues

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 module.exports = [
-  { q: "Where time lives?", answer: "CLOCK", clues: ["Ticks", "Has hands", "Everyone checks me"] },
-  { q: "I feed everyone", answer: "FOOD", clues: ["Smell", "Plates", "Students love me"] },
-  { q: "Silent knowledge", answer: "BOOK", clues: ["Shelves", "Study", "Library"] },
+  { q: "Where time?", answer: "CLOK", clues: ["Tics", "Has hands", "Everyone checks me"] },
+  { q: "I feed everyone", answer: "FOD", clues: ["Sell", "Plates", "Students love me"] },
+  { q: "Silent knowledge", answer: "BOK", clues: ["Shelves", "Study", "Library"] },
   { q: "Sweat & fun", answer: "PLAY", clues: ["Games", "Running", "Sports"] },
   { q: "Cars sleep here", answer: "PARK", clues: ["Vehicles", "Security", "Parking"] }
 ];
