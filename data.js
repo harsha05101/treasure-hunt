420a3603e1d0094771e960a788cedb18cd64e493
Harsha <harsha05101@gmail.com>
2025-12-31 15:08:31 +0530

    Fix finish UI development and sound extensions

--- data.js
+++ data.js
@@ -22,9 +22,9 @@ module.exports = [
     q: "A colour feather is painted beautifully with lots of colours on the wall.",
     answer: "PCK8",
     clues: [
-      "Inspiration of India’s national bird",
-      "The famous flute player wears this on his head",
-      "It is painted clearly on a wall"
+      "Inspiration of India’s national bird.",
+      "The famous flute player wears this on his head.",
+      "It is painted clearly on a wall."
     ]
   },
 
@@ -32,9 +32,9 @@ module.exports = [
     q: "I am last, but not the least.",
     answer: "RM10BLK",
     clues: [
-      "I am inside this community",
-      "I have 10 Doors",
-      "I am the smallest building in this Community"
+      "I am inside this community.",
+      "I have 10 Doors.",
+      "I am the smallest building in this Community."
     ]
   },
 
@@ -42,27 +42,27 @@ module.exports = [
     q: "Children gather and run around free here, I live in 10th House ",
     answer: "PLAR10",
     clues: [
-      "My house is covered in Sand",
-      "Swings and slides are present",
-      "1 to 9 are my neighbors"
+      "My house is covered in Sand.",
+      "Swings and slides are present.",
+      "1 to 9 are my neighbors."
     ]
   },
   {
     q: "I have stage but no roof, People love to gather and enjoy here.",
     answer: "OPNAD7",
     clues: [
-      "Every Person enjoys to be in my open space",
-      "Stairs, Grass and Trees are my family",
-      "Shyama and Megha are my Co-Neighbors"
+      "Every Person enjoys to be in my open space.",
+      "Stairs, Grass and Trees are my family.",
+      "Shyama and Megha are my Co-Neighbors."
     ]
   },
    {
     q: "I am the special person of this community, I have 4 Families with me.",
     answer: "CLBH4",
     clues: [
-      "People celebrate their Special Moments here ",
-      "Knowledge is my 2nd Family ",
-      "Gym is another family where people build their Fitness"
+      "People celebrate their Special Moments here.",
+      "Knowledge is my 2nd Family.",
+      "Gym is another family where people build their Fitness."
     ]
   },
   {
@@ -70,25 +70,25 @@ module.exports = [
     answer: "NL50BLK",
     clues: [
       "Morning's I am busy with many people surrounded around me making me into a new shape.",
-      "Bricks and Cement are my partners in crime",
-      "I will be a New Block to this community "
+      "Bricks and Cement are my partners in crime.",
+      "I will be a New Block to this community."
     ]
   },
   {
     q: "I am a place, where everyone loves to Chit-Chat and Gossip",
     answer: "3BNSPT",
     clues: [
-      "I have 3 Large Spaces to occupy",
-      "Its a corner spot",
-      "Most of the space is occupied my Ladies"
+      "I have 3 Large Spaces to occupy.",
+      "Its a corner spot.",
+      "Most of the space is occupied my Ladies."
     ]
   },
   { 
     q: "At Shree Krishna Heights, every visit begins with me. Welcoming everyone before homes come into sight.",
     answer: "SKH200",
     clues: [
-      "Every resident must cross this point",
-      "Vehicles slow down near me",
+      "Every resident must cross this point.",
+      "Vehicles slow down near me.",
       "I am covered in Green, it's my favourite colour."
     ]
   }
